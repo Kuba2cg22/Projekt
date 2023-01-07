4f467b4dbd087bece7130f148480dad5e529dddf
Jakub Pęk <01178575@pw.edu.pl>
2023-01-07 17:52:54 +0100

    hy

---
+++ A.py
@@ -0,0 +1,48 @@
+# +=''dD""{}
+
+
+lista = [1, 2, 3, 4, 5]
+
+# for i, x in enumerate(lista):
+#     if x == 4:
+#         lista = lista[0:i]
+
+# print(lista)
+
+def func():
+    for index, element in enumerate(lista):
+        is_end = bool(index + 1 == len(lista))
+        print(element, index, is_end)
+        if is_end:
+            func()
+
+
+# func()
+
+# i = 757
+
+
+# while i not in range(len(lista)):
+#     i -= len(lista)
+
+# print(i)
+
+# b = 'ASW;'
+
+# print(b.replace(';', ''))
+
+
+
+
+# def NOP():
+#     print(1)
+
+# a = 'NOP'
+
+# globals()[a]()
+
+c = [1, 2]
+
+u, i = c
+
+print(u,i)
